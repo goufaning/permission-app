e8ed5d6b96bd10e27ea5e8ca790db3b02c2d4c02
goufn <1031974286@qq.com>
2019-07-11 15:30:13 +0800

    readme

--- src/utils/global.js
+++ src/utils/global.js
@@ -5,13 +5,9 @@
  */
 
  // 后台管理系统服务器地址
-// export const baseUrl = 'http://139.196.87.48:8001'
-export const baseUrl = 'http://localhost:8080'
- // 系统数据备份还原服务器地址
-// export const backupBaseUrl = 'http://139.196.87.48:8002'
-export const backupBaseUrl = 'http://localhost:8080'
+export const baseUrl = 'http://123.206.82.234:8080'
+// export const baseUrl = 'http://localhost:8080'
 
 export default {
     baseUrl,
-    backupBaseUrl
 }
